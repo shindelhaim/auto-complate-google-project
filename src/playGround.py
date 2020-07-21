c72bfc1b05dddf2e2992fd92bef7617cb79c00f5
shindelhaim <sz@sem-wolf.org>
2020-07-22 00:03:16 +0300

    add mocdata

--- src/playGround.py
+++ src/playGround.py
@@ -17,6 +17,45 @@
 # return 
 
 
+#data
+# sentence1 = "Hello"
+# sentence2 = "Hi"
+
+# sorce = {1: "completion.py"}
+
+# sentences_sort = [{"sentence": "Hello", "src": 1}, {"sentence": "Hi", "src": 1}]
+
+# data = {
+#     "H": {
+#         "begin": 0,
+#         "end": 2
+#     },
+
+#     "He": {
+#         "begin": 0,
+#         "end": 1
+#     }, 
+
+#     "Hel": {
+#         "begin": 0,
+#         "end": 1
+#     }, 
+
+#     "Hell": {
+#         "begin": 0,
+#         "end": 1
+#     }, 
+
+#     "Hello": {
+#         "begin": 0,
+#         "end": 1
+#     }, 
+
+#     "Hi": {
+#         "begin": 1,
+#         "end": 2
+#     }
+# }
 
 
 
