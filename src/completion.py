7889ee06678e81baabc18031c4f24d686431e80e
shindelhaim <sz@sem-wolf.org>
2020-07-22 00:02:56 +0300

    use with data

--- src/completion.py
+++ src/completion.py
@@ -1,45 +1,8 @@
-sentence1 = "Hello"
-sentence2 = "Hi"
-
-sorce = {1: "completion.py"}
-
-sentences_sort = [{"sentence": "Hello", "src": 1}, {"sentence": "Hi", "src": 1}]
-
-data = {
-    "H": {
-        "begin": 0,
-        "end": 2
-    },
-
-    "He": {
-        "begin": 0,
-        "end": 1
-    }, 
-
-    "Hel": {
-        "begin": 0,
-        "end": 1
-    }, 
-
-    "Hell": {
-        "begin": 0,
-        "end": 1
-    }, 
-
-    "Hello": {
-        "begin": 0,
-        "end": 1
-    }, 
-
-    "Hi": {
-        "begin": 1,
-        "end": 2
-    }
-}
+from data.init_data import data_for_search, get_sentences_data
 
 
 def get_list_completions(input):
-    place = data.get(input, None)
+    place = data_for_search.get(input, None)
 
     if place is None:
         return []
@@ -49,10 +12,10 @@ def get_list_completions(input):
 
     index_end = index_begin + 5 if index_end - index_begin > 5 else index_end
 
-    return sentences_sort[index_begin:index_end]
+    return get_sentences_data()[index_begin:index_end]
 
 
-if __name__ == "__main__":
-    print(f'{get_list_completions("H")} \n')
-    print(f'{get_list_completions("Hel")} \n')
+# if __name__ == "__main__":
+#     print(f'{get_list_completions("H")} \n')
+#     print(f'{get_list_completions("Hel")} \n')
 
