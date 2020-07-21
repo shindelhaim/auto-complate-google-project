8381b8aba943a13a1396a127fedfe34da3f88f00
shindelhaim <sz@sem-wolf.org>
2020-07-21 21:58:09 +0300

    add read_from_file , get_sub_sentences

--- src/data/init_data.py
+++ src/data/init_data.py
@@ -1,8 +1,36 @@
-sorce_files = {1: "init_data.py"}
+sorce_files = {1: "src/data/contents.txt"}
 
-sentences_data = [{"sentence": "Hello", "src": 1}, {"sentence": "Hi", "src": 1}, {"sentence": "Hic", "src": 1},{"sentence": "ic", "src": 1}]
+sentences_data = []
 
 data = {}
+
+
+
+def remove_space_of_begin(sentence):
+    while(sentence and sentence[0] == ' '): 
+        sentence = sentence[1:]
+    
+    return sentence
+
+
+def get_dict_of_sentences(sentence, id_src):
+    sentence = remove_space_of_begin(sentence)
+    return [{"sentence": sentence, "src": id_src}]
+
+
+def read_from_files():
+    sentences_data_no_sorted = []
+    
+    for id, name in sorce_files.items():
+
+        with open(name) as file:
+            sentences = file.read().split("\n")
+
+        for sentence in sentences:
+            sentences_data_no_sorted += get_dict_of_sentences(sentence, id)
+
+    global sentences_data
+    sentences_data = sorted(sentences_data_no_sorted, key=lambda k: k["sentence"])
 
 
 def get_sub_sentences(sentence):
@@ -21,6 +49,7 @@ def init_data():
 
 
 if __name__ == "__main__":
+    read_from_files()
     init_data()
     print(f'{sentences_data} \n')
     print(f'{data} \n')
