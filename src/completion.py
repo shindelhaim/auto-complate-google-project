125c5ef6d60666260327b759274c1a6ccba012bf
shindelhaim <sz@sem-wolf.org>
2020-07-22 03:22:41 +0300

    update get_list_completions

--- src/completion.py
+++ src/completion.py
@@ -1,6 +1,9 @@
 from data.init_data import data_for_search, get_sentences_data
 
 
+
+
+
 def get_list_completions(input):
     place = data_for_search.get(input, None)
 
@@ -12,7 +15,12 @@ def get_list_completions(input):
 
     index_end = index_begin + 5 if index_end - index_begin > 5 else index_end
 
-    return get_sentences_data()[index_begin:index_end]
+    suitable_completions = get_sentences_data()[index_begin:index_end]
+
+    for item in suitable_completions:
+        item["score"] = len(item["sentence"]) * 2
+
+    return suitable_completions
 
 
 # if __name__ == "__main__":
