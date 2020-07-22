cf5623de3d346dfe591abdca041fc184d412d930
shindelhaim <sz@sem-wolf.org>
2020-07-22 14:45:00 +0300

    add character_replacement

--- src/input_correction.py
+++ src/input_correction.py
@@ -5,26 +5,53 @@ from data.init_data import data_for_search, get_sentences_data
 def character_replacement(substr, num_sentences):
     num_suitables = 0
     suitable_completions = []
+
     for i in range(0,len(substr),-1):
+        
         for char in string.printable:
 
             corrction_str = substr[:i] + char + substr[i + 1:]
-            place = data_for_search.get(corrction_str, None)
+            match_indexes = data_for_search.get(corrction_str, None)
             
-            if place:
-                index_begin = place["begin"]
-                index_end = place["end"]
+            if match_indexes:
                 
-                index_end = index_begin + num_sentences if num_suitables > num_sentences else index_end                
-                num_suitables += index_end - index_begin
+                match_indexes = match_indexes[:num_sentences] if len(match_indexes) > num_sentences else match_indexes
+                list_sentences_data = get_sentences_data()
 
-                nonlocal suitable_completions
-                suitable_completions = get_sentences_data()[index_begin:index_end]
-            if num_suitables > num_sentences:
-                break
+                for j in match_indexes:
+                    sentence_dict = list_sentences_data[j]
+                    sentence_dict["score"] = len(list_sentences_data[j]["sentence"]) * 2
 
-        if num_suitables > num_sentences:
-                break
+                    if(i < 4):
+                        sentence_dict["score"] -=  5 - i
+                    else:
+                        sentence_dict["score"] -= 1
 
-   
+                    suitable_completions += sentence_dict
 
+                num_suitables = len(suitable_completions)
+                num_sentences -= num_suitables
+            
+            if num_sentences == 0:
+                return suitable_completions
+    
+    return suitable_completions
+
+
+def deleting_character(substr, num_sentences):
+    return []
+
+def adding_character(substr, num_sentences):
+    return []
+
+def get_completions_with_correction(substr, num_sentences):
+    correction_list = []
+    correction_list += character_replacement(substr, num_sentences)
+    correction_list += deleting_character(substr, num_sentences)
+    correction_list += adding_character(substr, num_sentences)
+
+    correction_list = sorted(correction_list, key=lambda k: k["score"], reverse=True)
+    len_correction_list = len(correction_list)
+    index_end = 5 if len_correction_list > 5 else len_correction_list
+    
+    return correction_list[:index_end]
