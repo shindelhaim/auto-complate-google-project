39726391d0a9b219ac2dd476ab197424dd851c96
shindelhaim <sz@sem-wolf.org>
2020-07-23 16:23:00 +0300

    get_suitable_sentences_without_duplicates

--- src/input_correction.py
+++ src/input_correction.py
@@ -2,10 +2,55 @@ import string
 from data.init_data import data_for_search, get_sentences_data
 
 
+def get_suitable_sentences_without_duplicates(i_letter, corrction_str, match_indexes, num_miss_sentences, suitable_completions, type_change):
+    list_sentences_data = get_sentences_data()
+
+    for j in match_indexes:
+        if num_miss_sentences == 0:
+            return suitable_completions
+            
+        sentence_dict = list_sentences_data[j]
+        l = list(map(lambda d: {"sentence": d["sentence"], "src": d["src"], "line": d["line"]}, suitable_completions))
+
+        if sentence_dict in l:
+            continue
+
+        
+
+        if "replacement" == type_change:
+            sentence_dict["score"] = len(corrction_str) * 2
+            if(i_letter < 4):
+                sentence_dict["score"] -=  5 - i_letter
+            else:
+                sentence_dict["score"] -= 1
+
+        if "deleting" == type_change:
+            sentence_dict["score"] = len(corrction_str) * 2 - 1
+
+            if(i_letter < 4):
+                sentence_dict["score"] -=  10 - 2 * i_letter
+            else:
+                sentence_dict["score"] -= 2
+        
+        if  "adding" == type_change:
+            sentence_dict["score"] = len(corrction_str) * 2 + 1
+
+            if(i_letter < 4):
+                sentence_dict["score"] -=  10 - 2 * i_letter
+            else:
+                sentence_dict["score"] -= 2
+
+        sentence_dict["offset"] = (sentence_dict["sentence"]).index(corrction_str)
+        suitable_completions += [sentence_dict]
+        num_miss_sentences -= 1
+
+    return suitable_completions
+            
+
 def character_replacement(substr, num_sentences):
-    num_suitables = 0
     suitable_completions = []
-    
+    num_miss_sentences = num_sentences
+
     for i in range(0, len(substr))[::-1]:
 
         for char in string.printable.replace(substr[i],''):
@@ -13,44 +58,21 @@ def character_replacement(substr, num_sentences):
             corrction_str = substr[:i] + char + substr[i + 1:] if i != len(substr) - 1 else substr[:i] + char
             match_indexes = data_for_search.get(corrction_str, None)
             
-            if match_indexes:
-                
-                some_match_indexes = match_indexes[:num_sentences] if len(match_indexes) > num_sentences else match_indexes
-                count_add_later = num_sentences
-                list_sentences_data = get_sentences_data()
+            if not match_indexes:
+                continue
 
-                for j in some_match_indexes:
-                    sentence_dict = list_sentences_data[j]
-                    
-                    while count_add_later < len(match_indexes) and sentence_dict in list(map(lambda d: {"sentence": d["sentence"], "src": d["src"], "line": d["line"]}, suitable_completions)):
-                        sentence_dict = list_sentences_data[count_add_later]
-                        count_add_later += 1
-                    
-                    if count_add_later < len(match_indexes):
-                        break
-
-                    sentence_dict["score"] = len(list_sentences_data[j]["sentence"]) * 2
-
-                    if(i < 4):
-                        sentence_dict["score"] -=  5 - i
-                    else:
-                        sentence_dict["score"] -= 1
-
-                    sentence_dict["offset"] = (sentence_dict["sentence"]).index(corrction_str)
-                    suitable_completions += [sentence_dict]
-
-                num_suitables = len(suitable_completions)
-                num_sentences -= num_suitables
+            suitable_completions = get_suitable_sentences_without_duplicates(i, corrction_str, match_indexes, num_miss_sentences, suitable_completions, "replacement")
+            num_miss_sentences = num_sentences - len(suitable_completions)
             
-            if num_sentences == 0:
+            if num_miss_sentences == 0:
                 return suitable_completions
     
     return suitable_completions
 
 
 def deleting_character(substr, num_sentences):
-    num_suitables = 0
     suitable_completions = []
+    num_miss_sentences = num_sentences
     
     for i in range(0, len(substr) - 1)[::-1]:
 
@@ -58,79 +80,35 @@ def deleting_character(substr, num_sentences):
             
             corrction_str = substr[:i + 1] + char + substr[i + 1:]
             match_indexes = data_for_search.get(corrction_str, None)
+
+            if not match_indexes:
+                continue
             
-            if match_indexes:
-                
-                some_match_indexes = match_indexes[:num_sentences] if len(match_indexes) > num_sentences else match_indexes
-                count_add_later = num_sentences
-                list_sentences_data = get_sentences_data()
-
-                for j in some_match_indexes:
-                    sentence_dict = list_sentences_data[j]
-                    
-                    while count_add_later < len(match_indexes) and sentence_dict in list(map(lambda d: {"sentence": d["sentence"], "src": d["src"], "line": d["line"]}, suitable_completions)):
-                        sentence_dict = list_sentences_data[count_add_later]
-                        count_add_later += 1
-                    
-                    if count_add_later < len(match_indexes):
-                        break
-
-                    sentence_dict["score"] = len(list_sentences_data[j]["sentence"]) * 2
-
-                    if(i < 4):
-                        sentence_dict["score"] -=  10 - 2 * i
-                    else:
-                        sentence_dict["score"] -= 2
-
-                    sentence_dict["offset"] = (sentence_dict["sentence"]).index(corrction_str)
-                    suitable_completions += [sentence_dict]
-
-                num_suitables = len(suitable_completions)
-                num_sentences -= num_suitables
+            suitable_completions = get_suitable_sentences_without_duplicates(i, corrction_str, match_indexes, num_miss_sentences, suitable_completions, "deleting")
+            num_miss_sentences = num_sentences - len(suitable_completions)
             
-            if num_sentences == 0:
+            if num_miss_sentences == 0:
                 return suitable_completions
     
     return suitable_completions
 
 
 def adding_character(substr, num_sentences):
-    num_suitables = 0
     suitable_completions = []
-    
+    num_miss_sentences = num_sentences
+
     for i in range(0, len(substr))[::-1]:
         
         corrction_str = substr[:i] + substr[i + 1:] if i != len(substr) - 1 else substr[:i]
         match_indexes = data_for_search.get(corrction_str, None)
             
-        if match_indexes:
-            some_match_indexes = match_indexes[:num_sentences] if len(match_indexes) > num_sentences else match_indexes
-            count_add_later = num_sentences
-            list_sentences_data = get_sentences_data()
+        if not match_indexes:   
+            continue
 
-            for j in some_match_indexes:
-                sentence_dict = list_sentences_data[j]
-                while (count_add_later < len(match_indexes)) and sentence_dict in list(map(lambda d: {"sentence": d["sentence"], "src": d["src"], "line": d["line"]}, suitable_completions)):
-                    sentence_dict = list_sentences_data[count_add_later]
-                    count_add_later += 1
-                
-                if count_add_later < len(match_indexes):
-                    break
-                
-                sentence_dict["score"] = len(list_sentences_data[j]["sentence"]) * 2
-
-                if(i < 4):
-                    sentence_dict["score"] -=  10 - 2 * i
-                else:
-                    sentence_dict["score"] -= 2
-
-                sentence_dict["offset"] = (sentence_dict["sentence"]).index(corrction_str)
-                suitable_completions += [sentence_dict]
-
-        num_suitables = len(suitable_completions)
-        num_sentences -= num_suitables
+        suitable_completions = get_suitable_sentences_without_duplicates(i, corrction_str, match_indexes, num_miss_sentences, suitable_completions, "adding")
+        num_miss_sentences = num_sentences - len(suitable_completions)
             
-        if num_sentences == 0:
+        if num_miss_sentences == 0:
             return suitable_completions
     
     return suitable_completions
@@ -148,7 +126,7 @@ def get_completions_with_correction(substr, num_sentences):
     new_correction_list = []
 
     for item in correction_list:
-        if {"sentence": item["sentence"], "src": item["src"], "line": item["line"]} not in list(map(lambda d: {"sentence": d["sentence"], "src": d["src"], "line": d["line"]}, new_correction_list)):
+        if {"sentence": item["sentence"], "src": item["src"], "line": item["line"]} not in list(map(lambda d: {"sentence": d["sentence"], "src": d["src"], "line": item["line"]}, new_correction_list)):
             new_correction_list += [item]
             
     len_correction_list = len(new_correction_list)
