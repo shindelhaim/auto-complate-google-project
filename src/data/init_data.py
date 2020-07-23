2977f81458afa614d5c2a0806ec0c34cdfceffc8
shindelhaim <sz@sem-wolf.org>
2020-07-23 12:39:09 +0300

    add init_source_files

--- src/data/init_data.py
+++ src/data/init_data.py
@@ -1,7 +1,7 @@
 import string
+import os
 
-
-source_files = {1: "data/contents.txt"}
+source_files = {0: "data/contents.txt"}
 
 sentences_data = []
 
@@ -15,6 +15,22 @@ def get_source_files():
 
 def get_sentences_data():
     return sentences_data
+
+id_file = 1 
+
+def init_source_files(parent):
+    global id_file
+
+    for file_name in os.listdir(parent):
+        
+        if file_name.endswith(".txt"):
+            source_files[id_file] = "".join((parent, "/", file_name))
+            id_file += 1
+        
+        else:
+            current_path = "".join((parent, "/", file_name))
+            if os.path.isdir(current_path):
+                init_source_files(current_path)
 
 
 def remove_space_of_begin(sentence):
@@ -30,7 +46,7 @@ def get_dict_of_sentences(sentence, id_src, num_line):
 
 
 def read_from_files():  
-    for id, name in source_files.items():
+    for id, name in list(source_files.items())[:3]:
         global sentences_data
 
         with open(name) as file:
@@ -65,14 +81,17 @@ def init_data_for_search():
 
 
 def init_meta_data():
+    init_source_files("data/technology_texts")
     read_from_files()
     init_data_for_search()
 
 
 if __name__ == "__main__":
-    init_meta_data()
-    # print(f'{sentences_data} \n')
-    print(f'{data_for_search["C server"]} \n')
+    init_source_files("data/technology_texts")
+    print(source_files)
+    # init_meta_data()
+    # # print(f'{sentences_data} \n')
+    # print(f'{data_for_search["C server"]} \n')
 
-    for i in data_for_search["C server"]:
-        print(f'{sentences_data[i]} \n')
+    # for i in data_for_search["C server"]:
+    #     print(f'{sentences_data[i]} \n')
