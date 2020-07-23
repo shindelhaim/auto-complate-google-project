6212f443eb44cf3f1bf2abaf59e0c5cf72b70570
shindelhaim <sz@sem-wolf.org>
2020-07-23 03:57:31 +0300

    add num line

--- src/auto_complete_data.py
+++ src/auto_complete_data.py
@@ -2,7 +2,7 @@ class AutoCompleteData:
     
     def __init__(self, data):
         self.completed_sentence = data["sentence"]
-        self.source_text = data["src"]
+        self.source_text = [data["src"], data["line"]]
         self.offset = data["offset"]
         self.score = data["score"]
 
