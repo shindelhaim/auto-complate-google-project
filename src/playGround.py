859c53fb1be4adb457f805c7e3f87101a3847c0f
shindelhaim <sz@sem-wolf.org>
2020-07-23 12:40:38 +0300

    add trying

--- src/playGround.py
+++ src/playGround.py
@@ -1,4 +1,37 @@
 
+
+
+# import os
+
+# i = 0
+
+# def scan_folder(parent):
+#     # iterate over all the files in directory 'parent'
+#     for file_name in os.listdir(parent):
+#         if file_name.endswith(".txt"):
+#             # if it's a txt file, print its name (or do whatever you want)
+#             global i
+#             print(i,file_name)
+#             i += 1
+#         else:
+#             current_path = "".join((parent, "/", file_name))
+#             if os.path.isdir(current_path):
+#                 # if we're checking a sub-directory, recall this method
+#                 scan_folder(current_path)
+
+# scan_folder("data/technology_texts")
+
+# import glob
+
+# list_ = []
+
+# path = 'data/python-3.8.4-docs-text'
+# for filename in glob.glob('data/python-3.8.4-docs-text/*/*.txt'):
+#    list_ += [filename]
+
+# print(list_, len(list_))
+
+
 # page = {}
 
 # input = "hello big"
