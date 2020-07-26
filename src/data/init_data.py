f9ff3b1a92857a75d4604528a9ca201b329baa0b
shindelhaim <sz@sem-wolf.org>
2020-07-26 03:42:06 +0300

    add ignore_punctuation_and_spaces

--- src/data/init_data.py
+++ src/data/init_data.py
@@ -1,8 +1,10 @@
 import string
 import os
-import shelve
+
 
 source_files = {0: "data/contents.txt"}
+
+id_file = 1 
 
 sentences_data = []
 
@@ -10,18 +12,17 @@ data_for_search = {}
 
 
 
+def get_sentences_data():
+    return sentences_data
+
+
 def get_source_files():
     return source_files
 
 
-def get_sentences_data():
-    return sentences_data
-
-id_file = 1 
-
 def init_source_files(parent):
     global id_file
-
+    
     for file_name in os.listdir(parent):
         
         if file_name.endswith(".txt"):
@@ -47,7 +48,7 @@ def get_dict_of_sentences(sentence, id_src, num_line):
 
 
 def read_from_files():  
-    for id, name in list(source_files.items())[:10]:
+    for id, name in list(source_files.items())[:1]:
         global sentences_data
 
         with open(name) as file:
@@ -60,27 +61,16 @@ def read_from_files():
                 sentences_data += [dict_sentence]
 
 
-# def read_from_files():  
-#     key_sentence = 0
-#     sentences_data = shelve.open('sentences_data.db')
-    
-#     try:
-#         for id, name in list(source_files.items())[:10]:
-#             with open(name) as file:
-#                 sentences = file.read().split("\n")
-
-#             for num_line, sentence in enumerate(sentences):
-#                 dict_sentence = get_dict_of_sentences(sentence, id, num_line)
-
-#                 if dict_sentence["sentence"] != "":
-#                     sentences_data[str(key_sentence)] = dict_sentence
-#                     key_sentence += 1
-#     finally:
-#         sentences_data.close()
-
-
 def get_sub_sentences(sentence):
-    return [sentence[i:j] for i in range(0, len(sentence) + 1) for j in range(i + 1, len(sentence) + 1)]
+    return [sentence[i:j] for i in range(0, len(sentence)) for j in range(i + 1, len(sentence) + 1)]
+
+
+def ignore_punctuation_and_spaces(str):
+    str = str.lower()
+    exclude = set(string.punctuation)
+    str = ''.join(ch for ch in str if ch not in exclude)        
+    str = str.split(' ')
+    return ' '.join([c for c in str if c != ''])
 
 
 def init_data_for_search():
@@ -88,16 +78,17 @@ def init_data_for_search():
         sub_sentences = get_sub_sentences(item["sentence"])
     
         for sub in sub_sentences:
-            if sub in data_for_search:
-                data_for_search[sub].add(index)
-            else:
+            sub = ignore_punctuation_and_spaces(sub)
+
+            if sub not in data_for_search:
                 data_for_search[sub] = {index}
-    
-    #sort indexes for every sub
+            else:
+                data_for_search[sub].add(index)
+                
+
     for sub in data_for_search.keys():
-        data_for_search[sub] = list(data_for_search[sub])
         data_for_search[sub] = sorted(data_for_search[sub], 
-            key=lambda k: sentences_data[k]["sentence"][(sentences_data[k]["sentence"]).index(sub):])
+            key=lambda k: sentences_data[k]["sentence"])
 
 
 def init_meta_data():
@@ -106,19 +97,12 @@ def init_meta_data():
     init_data_for_search()
 
 
-if __name__ == "__main__":
-    init_source_files("data/technology_texts")
-    read_from_files()
-    s = shelve.open('sentences_data.db')
-try:
-    existing = s['25']
-    existing2 = s['1054']
-finally:
-    s.close()
+# if __name__ == "__main__":
+    # init_source_files("data/technology_texts")
+    # print(source_files)
 
-print(existing, existing2)
     # init_meta_data()
-    # # print(f'{sentences_data} \n')
+    # print(f'{sentences_data} \n')
     # print(f'{data_for_search["C server"]} \n')
 
     # for i in data_for_search["C server"]:
