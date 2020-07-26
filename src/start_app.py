11f717f954791b50f7c7691c1d6df63590894356
shindelhaim <sz@sem-wolf.org>
2020-07-26 03:39:55 +0300

    change in print

--- src/start_app.py
+++ src/start_app.py
@@ -1,4 +1,8 @@
 from completion import get_best_k_completions
+from data.init_data import ignore_punctuation_and_spaces, get_source_files
+from auto_complete_data import AutoCompleteData
+
+
 
 def start_application():
     print("The system is ready:")
@@ -9,8 +13,12 @@ def start_application():
 
         while(new_input != '#' and user_input != '#'):
             user_input += new_input
-            print(f'Here are suggestions: \n')
-            get_best_k_completions(user_input)
-            
+            autoCompleteData_list = get_best_k_completions(ignore_punctuation_and_spaces(user_input))
+            print(f'Here are {len(autoCompleteData_list)} suggestions: \n')
+
+            for i in range(len(autoCompleteData_list)):
+                print(f'{i + 1}. {autoCompleteData_list[i].completed_sentence},') 
+                print(f'(src: {get_source_files()[autoCompleteData_list[i].source_text]}, offset: {autoCompleteData_list[i].offset}, score: {autoCompleteData_list[i].score}) \n')
+
             print(user_input, end='')
             new_input =  input()
